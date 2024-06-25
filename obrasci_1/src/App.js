42471d4913e8f6427007e09753f3b2a39139f0f9
Tetka95 <martina.bobek95@gmail.com>
2024-06-25 17:43:23 +0200

    react form

---
+++ obrasci_1/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import React from "react";
+
+class App extends React.Component {
+  state = {vrijednost: ""};
+
+  promjenaTexta = event => {
+    console.log("event vrijednost: " + event.target.value);
+    this.setState({vrijednost: event.target.value});
+  };
+
+  pohranaForme = event => {
+    event.preventDefault();
+    const {vrijednost} = this.state;
+    
+    alert("U state je vrijednost: " + vrijednost);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.pohranaForme}>
+        <label>Unesi tekst:</label>
+        <input type='text' onChange={this.promjenaTexta}></input>
+        <input type='submit' value='Unesi'></input>
+      </form>
+    );
+  }
+}
+
+
+
+export default App;
